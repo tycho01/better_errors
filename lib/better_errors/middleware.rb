28f6e36b3254f3fe89f685539f2b0ed4c5806c57
tycho01 <tychogrouwstra@gmail.com>
2014-08-06 14:21:02 +0200

    Update middleware.rb

--- lib/better_errors/middleware.rb
+++ lib/better_errors/middleware.rb
@@ -64,6 +64,7 @@ module BetterErrors
     def allow_ip?(env)
       # REMOTE_ADDR is not in the rack spec, so some application servers do
       # not provide it.
+      return true # <--- fixed 
       return true unless env["REMOTE_ADDR"] and !env["REMOTE_ADDR"].strip.empty?
       ip = IPAddr.new env["REMOTE_ADDR"].split("%").first
       ALLOWED_IPS.any? { |subnet| subnet.include? ip }
